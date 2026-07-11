d697892a7f6c830a668c379a3ca0e93943df2f52
Somnath21-hub <somnath10235@gmail.com>
2026-07-11 22:17:53 +0530

    Remove Designed and Developed attribution from footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,9 +5,6 @@ const Footer = () => {
     <footer>
       <p>© {new Date().getFullYear()} Somnath's Physique. All Rights Reserved.</p>
       <p>Address: Newtown, Coal India, Kolkata</p>
-      <p style={{ fontSize: "12px", marginTop: "5px", opacity: 0.7 }}>
-        Designed And Developed By Mudabbir
-      </p>
     </footer>
   );
 }
