68774ab925993e62e734327d40ca50678bd4d3eb
Somnath21-hub <somnath10235@gmail.com>
2026-08-06 21:39:13 +0530

    Fix pre-save hook to not call next() in async middleware

--- backend/models/User.js
+++ backend/models/User.js
@@ -53,9 +53,9 @@ const userSchema = new mongoose.Schema({
 });
 
 // Encrypt password before saving
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) {
-    next();
+    return;
   }
   this.password = await bcrypt.hash(this.password, 10);
 });
